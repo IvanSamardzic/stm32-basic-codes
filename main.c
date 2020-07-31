adf8eae7f3884c8602609cb115c471435340bf8c
Ivan Samardzic <56680446+IvanSamardzic@users.noreply.github.com>
2020-07-31 19:37:57 +0200

    ADC tests

--- main.c
+++ main.c
@@ -1,31 +1,42 @@
-//PD12, PD13, PD14, PD15
-//APB1
-//RCC -> AHB1ENR 
-//GPIODEN (bit3) in AHB1ENR
-//GPIOD MODER, GPIOD ODR
+// Read the ADC data register value
+// Set pot middle pin on PA0
 
 #include "stm32f4xx.h"                  // Device header
-void delayMs(int delay);
-
 int main(void){
-	// Enable the clock access to GPIOD
-	RCC->AHB1ENR |= 0x8;
-	// Set PD12, PD13, PD14, PD15 as output pins
-	// 0x55000000 -> 0b 0101 0101 0000 0000 0000 0000 0000 0000
-	GPIOD->MODER |= 0x55000000;
+	// Local variable definitions
+	uint32_t analogValue;
+	
+	// Enable clock to GPIOA
+	RCC->AHB1ENR |= 0x1;
+	
+	// Set PA1 as analog input pin and PA2, PA0 as output pin
+	GPIOA->MODER |= 0x1D;
+	
+	// Enable clock to ADC1
+	RCC->APB2ENR |= 0x100;
+	
+	// Disable ADC and set the sequence
+	ADC1->CR2 = 0;
+	ADC1->SQR3 = 1;
+	
+	// Enable ADC and declare continuous conversion mode
+	ADC1->CR2 |= 0x3;
+	
 	while(1){
-		// Turn on the LED connected to PD12
-		GPIOD->ODR |= 0x1000; 
-		delayMs(1000);
-		GPIOD->ODR &= 0xEFFF;
-		delayMs(1000);
+		// Start the conversion
+		ADC1->CR2 |= 0x40000000;
+		
+		// Write result into a variable
+		analogValue= ADC1->DR;
+		
+		// Check ADC1->ODR value
+		if(ADC1->DR > 0x0800){
+		
+			// Turn on the LED
+			GPIOA->ODR = 0x1;
+		}
+		if(ADC1->DR < 0x0800){
+			GPIOA->ODR = 0x4;
+		}
 	}
 }
-
-void delayMs(int delay){
-	int i;
-	for(; delay>0 ; delay--){
-		for(i=0; i<3195; i++){}
-	}
-}
-
